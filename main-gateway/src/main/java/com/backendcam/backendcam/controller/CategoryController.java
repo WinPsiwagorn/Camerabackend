5793ed162700ac4c959086af3bcbd6697bd56c3e
ABOUTBLANK <6631503017@lamduan.mfu.ac.th>
2026-02-17 21:07:39 +0700

    fix: update firebase credentials path in application.properties for correct file reference

--- main-gateway/src/main/java/com/backendcam/backendcam/controller/CategoryController.java
+++ main-gateway/src/main/java/com/backendcam/backendcam/controller/CategoryController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +62,7 @@ public class CategoryController {
         }
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> updateCategory(@PathVariable int id, @RequestBody Category category) {
         try {
             Category updated = categoryService.updateCategory(id, category);
